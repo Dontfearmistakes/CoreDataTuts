3a6a56d3a9ed15d75d43eec814246ddaed226efc
Dontfearmistakes <maximebernardo@gmail.com>
2014-02-13 19:16:15 +0100

    Initial Commit

---
+++ CoreDataTutorial2/FailedBankDetails.m
@@ -0,0 +1,20 @@
+//
+//  FailedBankDetails.m
+//  FailedBankCD
+//
+//  Created by Max on 13/02/14.
+//  Copyright (c) 2014 Max. All rights reserved.
+//
+
+#import "FailedBankDetails.h"
+#import "FailedBankInfo.h"
+
+
+@implementation FailedBankDetails
+
+@dynamic zip;
+@dynamic closeDate;
+@dynamic updateDate;
+@dynamic info;
+
+@end
---
+++ CoreDataTutorial2/FailedBankInfo.m
@@ -0,0 +1,20 @@
+//
+//  FailedBankInfo.m
+//  FailedBankCD
+//
+//  Created by Max on 13/02/14.
+//  Copyright (c) 2014 Max. All rights reserved.
+//
+
+#import "FailedBankInfo.h"
+#import "FailedBankDetails.h"
+
+
+@implementation FailedBankInfo
+
+@dynamic name;
+@dynamic city;
+@dynamic state;
+@dynamic details;
+
+@end
